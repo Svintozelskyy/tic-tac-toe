b9c968468402abdbe9b9aec3e14a86747cdf0cc5
Svintozelskyy <svintoz@gmail.com>
2022-06-17 19:58:11 +0300

    Convert CellNumberConverter to interface

--- src/academy/devonline/tictactoe/component/keypad/DesktopNumericKeypadCellNumberConverter.java
+++ src/academy/devonline/tictactoe/component/keypad/DesktopNumericKeypadCellNumberConverter.java
@@ -15,16 +15,37 @@
  *
  */
 
-package academy.devonline.tictactoe.component;
+package academy.devonline.tictactoe.component.keypad;
 
+import academy.devonline.tictactoe.component.CellNumberConverter;
 import academy.devonline.tictactoe.model.Cell;
 
 /**
  * @author devonline
  * @link http://devonline.academy/java
  */
-public interface CellNumberConverter {
+public class DesktopNumericKeypadCellNumberConverter implements CellNumberConverter {
 
-   Cell toCell(char number);
-   char toNumber(Cell cell);
+    private final char[][] mapping = {
+            {'7', '8', '9'},
+            {'4', '5', '6'},
+            {'1', '2', '3'}
+    };
+
+    @Override
+    public Cell toCell(final char number){
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (mapping[i][j] == number) {
+                    return new Cell(i, j);
+                }
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public char toNumber(final Cell cell){
+        return mapping[cell.getRow()][cell.getCol()];
+    }
 }
